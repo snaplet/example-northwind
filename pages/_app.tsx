75bde4d1b68beff3bda6660be3dcb5d01bbd2918
jgoux <hi@jgoux.dev>
2022-10-05 15:29:16 +0200

    comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// comment
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
