c4b6ba9b25b02d9f93cd95ab57307b13447980d9
Peter Pistorius <peter.pistorius@gmail.com>
2023-08-01 15:15:48 +0200

    Stash.

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const Home: NextPage<{ tables: { table_name: string }[] }> = ({ tables }) => {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const tables: { table_name: string }[] =
-    await db.$queryRaw`select table_name from information_schema.tables where table_schema not in ('information_schema', 'pg_catalog') and table_type = 'BASE TABLE'`;
+    await db.$queryRaw`select table_schema, table_name from information_schema.tables where table_schema not in ('information_schema', 'pg_catalog') and table_type = 'BASE TABLE'`;
 
   return {
     props: {
